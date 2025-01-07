f2ffef930213a8c3dd224ed9c217f50e6817aa50
Gabriel Pires <gabpiresdev@gmail.com>
2025-01-07 18:03:14 -0300

    chore: setup application

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, Text, View } from 'react-native'
+import {
+  Roboto_700Bold,
+  Roboto_400Regular,
+  useFonts,
+} from '@expo-google-fonts/roboto'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [fontsLoaded] = useFonts({
+    Roboto_700Bold,
+    Roboto_400Regular,
+  })
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: '#202024',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor={'transparent'}
+        translucent
+      />
+      {fontsLoaded ? <Text>Hello World!</Text> : <Text>Loading...</Text>}
+    </View>
+  )
+}
