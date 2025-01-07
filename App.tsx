d1ceb7a47f13d33885f43b46ff2117faa016dd50
Gabriel Pires <gabpiresdev@gmail.com>
2025-01-07 18:10:53 -0300

    feat: integrate Gluestack UI and update dependencies

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
-import { StatusBar, Text, View } from 'react-native'
+import { StatusBar, View } from 'react-native'
 import {
   Roboto_700Bold,
   Roboto_400Regular,
   useFonts,
 } from '@expo-google-fonts/roboto'
+import { GluestackUIProvider, Text } from '@gluestack-ui/themed'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -12,20 +13,28 @@ export default function App() {
   })
 
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: '#202024',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor={'transparent'}
-        translucent
-      />
-      {fontsLoaded ? <Text>Hello World!</Text> : <Text>Loading...</Text>}
-    </View>
+    <GluestackUIProvider>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: '#202024',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor={'transparent'}
+          translucent
+        />
+        {fontsLoaded ? (
+          <Text color="white" fontSize={24}>
+            Hello World!
+          </Text>
+        ) : (
+          <Text>Loading...</Text>
+        )}
+      </View>
+    </GluestackUIProvider>
   )
 }
