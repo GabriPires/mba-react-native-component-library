09cf3dd3dab17365458ac76a62fac8ef1c8b49f0
Gabriel Pires <gabpiresdev@gmail.com>
2025-01-09 18:32:28 -0300

    feat: update SignIn screen Input fields with email keyboard type and secure text entry

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -28,9 +28,13 @@ export function SignIn() {
         <Center gap="$2">
           <Heading color="$gray100">Acesse a conta</Heading>
 
-          <Input placeholder="E-mail" />
+          <Input
+            placeholder="E-mail"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
 
-          <Input placeholder="Senha" />
+          <Input placeholder="Senha" secureTextEntry />
         </Center>
       </VStack>
     </VStack>
