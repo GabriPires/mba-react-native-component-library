520c2bd8027ab14a20aca9b187dcf9dbbd5701b3
Gabriel Pires <gabpiresdev@gmail.com>
2025-01-22 19:44:37 -0300

    feat: define type for authentication routes and enhance navigator configuration

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,12 +1,22 @@
 import { SignIn } from '@/screens/SignIn'
 import { SignUp } from '@/screens/SignUp'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import {
+  createNativeStackNavigator,
+  NativeStackNavigationProp,
+} from '@react-navigation/native-stack'
 
-const { Navigator, Screen } = createNativeStackNavigator()
+type AuthRoutes = {
+  'sign-in': undefined
+  'sign-up': undefined
+}
+
+export type AuthNavigatorRoutesProps = NativeStackNavigationProp<AuthRoutes>
+
+const { Navigator, Screen } = createNativeStackNavigator<AuthRoutes>()
 
 export function AuthRoutes() {
   return (
-    <Navigator>
+    <Navigator screenOptions={{ headerShown: false }}>
       <Screen name="sign-in" component={SignIn} />
       <Screen name="sign-up" component={SignUp} />
     </Navigator>
