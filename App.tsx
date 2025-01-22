2f9492023155c6493a018fe6b9709bbb1156c75e
Gabriel Pires <gabpiresdev@gmail.com>
2025-01-22 19:48:54 -0300

    feat: refactor App component to use Routes for navigation and add routing structure

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import {
 import { GluestackUIProvider } from '@gluestack-ui/themed'
 import { config } from './config/gluestack-ui.config'
 import { Loading } from '@/components/Loading'
-import { SignUp } from '@/screens/SignUp'
+import { Routes } from '@/routes'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -22,7 +22,7 @@ export default function App() {
         backgroundColor={'transparent'}
         translucent
       />
-      {fontsLoaded ? <SignUp /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </GluestackUIProvider>
   )
 }
